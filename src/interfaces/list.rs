faa8c84882884b9c18aef141e98d18fde132a2f9
David Roussov <david.roussov@gmail.com>
2024-02-05 16:16:54 +1100

    convert json array items to lines and print to screen

--- src/interfaces/list.rs
+++ src/interfaces/list.rs
@@ -27,12 +27,12 @@ use crossterm::{
     },
     Result,
 };
-use std::{io::stdout};
 use std::io::{self, Write};
 use serde_json::Value;
+use std::{io::stdout, time::Duration, time::Instant};
 
 pub fn start_list_interface(stdout: &mut io::Stdout, json: Value) -> Result<()> {
-    log::trace!("In list to terminal");
+    log::trace!("In start_list_interface");
 
     let size = terminal::size()?;
     let terminal_y = &size.1;
@@ -43,12 +43,163 @@ pub fn start_list_interface(stdout: &mut io::Stdout, json: Value) -> Result<()> 
     let padding_y: u16 = 1; // does tmux status bar take up one row
     log::debug!("padding_x: {}, padding_y: {}", padding_x, padding_y);
 
-    let items = json["items"].as_array() else {
-        log::error!("items is not an array");
-        return Ok(());
-    };
+    let lines = get_lines(&json, *terminal_y, *terminal_x, padding_y, padding_x);
 
-    log::debug!("{:?}", items);
+    let mut offset: usize = 0;
+    let mut page: Vec<String> = get_page(&lines, *terminal_y, padding_y, offset);
+
+    print_page_to_screen(stdout, padding_x, padding_y, page);
+
+    let mut last_char = None;
+    let mut last_time = Instant::now();
+
+    loop {
+        if poll(Duration::from_millis(1_000))? {
+            let event = read()?;
+
+            if event == Event::Key(KeyCode::Char('q').into()) {
+                break;
+            }
+
+            if event == Event::Key(KeyCode::Char('j').into()) {
+                clear_screen(stdout);
+
+                page = get_page(&lines, *terminal_y, padding_y, offset);
+                print_page_to_screen(stdout, padding_x, padding_y, page);
+
+                last_char = Some('j');
+                last_time = Instant::now();
+            }
+
+            if event == Event::Key(KeyCode::Char('k').into()) {
+                clear_screen(stdout);
+
+                page = get_page(&lines, *terminal_y, padding_y, offset);
+                print_page_to_screen(stdout, padding_x, padding_y, page);
+
+                last_char = Some('k');
+                last_time = Instant::now();
+            }
+
+            if event == Event::Key(KeyCode::Char('g').into()) {
+
+                if last_char == Some('g') && last_time.elapsed() < Duration::from_millis(500) {
+                    clear_screen(stdout);
+                }
+
+                last_char = Some('g');
+                last_time = Instant::now();
+            }
+
+            if event == Event::Key(KeyCode::Char('G').into()) {
+                clear_screen(stdout);
+
+                last_char = Some('G');
+                last_time = Instant::now();
+            }
+        }
+    }
 
     Ok(())
+}
+
+
+fn clear_screen(stdout: &mut io::Stdout) -> Result<()> {
+    let size = terminal::size()?;
+
+    for y in 0..size.1 {
+        for x in 0..size.0 {
+            stdout
+                .queue(cursor::MoveTo(x,y))?
+                .queue(style::PrintStyledContent( "█".white()))?;
+        }
+    }
+    stdout.flush()?;
+
+    Ok(())
+}
+
+fn print_page_to_screen(
+    stdout: &mut io::Stdout,
+    padding_x: u16,
+    padding_y: u16,
+    page: Vec<String>,
+) -> Result<()> {
+    log::trace!("In print_page_to_screen");
+
+    let mut x = padding_x;
+    let mut y = padding_y;
+
+    for line in page.iter() {
+
+        log::debug!("line: {}", line);
+
+        stdout
+            .queue(cursor::MoveTo(x, y))?
+            .queue(style::PrintStyledContent(
+                line.clone()
+                .with(Color::Black)
+                .on(Color::White)
+            ))?;
+
+        y += 1;
+    }
+
+    Ok(())
+}
+
+fn get_page(lines: &Vec<String>, terminal_y: u16, padding_y: u16, offset: usize) -> Vec<String> {
+    log::trace!("In get_page");
+
+    let max_lines = (terminal_y - 2 * padding_y) as usize;
+    log::debug!("max_lines: {}", max_lines);
+
+    return lines[offset..max_lines + offset].to_vec();
+}
+
+fn get_lines(
+    json: &Value,
+    terminal_y: u16, 
+    terminal_x: u16,
+    padding_y: u16,
+    padding_x: u16,
+) -> Vec<String> {
+    log::trace!("In get_lines");
+
+    let mut lines: Vec<String> = Vec::new();
+
+    let Some(items) = json["items"].as_array() else {
+        log::debug!("json items is not an array");
+        return Vec::new();
+    };
+
+    let chunk_size = (terminal_x - 2 * padding_x) as usize;
+    log::debug!("chunk_size: {}", chunk_size);
+
+    for item in items.iter() {
+        if let Some(obj_map) = item["data"].as_object() {
+            let mut current_line: String = "".to_string();
+
+            for (key, value) in obj_map.iter() {
+                log::debug!("key: {}", key);
+                log::debug!("value: {}", value);
+
+                let segment = format!("{}: {} ", key, value);
+
+                if segment.len() > chunk_size {
+                    log::info!("Segment length is greater than screen width");
+
+                } else if current_line.len() + segment.len() < chunk_size {
+                    current_line += &segment;
+                } else if current_line.len() + segment.len() > chunk_size {
+                    lines.push(current_line);
+                    current_line = segment;
+                }
+            }
+        }
+
+        lines.push("".to_string());
+    }
+
+    return lines;
 }
