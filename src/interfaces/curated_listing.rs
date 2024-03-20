d9034074ba5651b21de99a9767890fc3fc143a94
David Roussov <david.roussov@gmail.com>
2024-03-20 12:33:27 +1100

    generic stateful list, display and select urls from active list item

--- src/interfaces/curated_listing.rs
+++ src/interfaces/curated_listing.rs
@@ -17,9 +17,9 @@ type Err = Box<dyn std::error::Error>;
 type Result<T> = std::result::Result<T, Err>;
 
 #[derive(Debug, Default)]
-struct StatefulList {
+struct StatefulList<T> {
     state: ListState,
-    items: Vec<pandoculation::CuratedListingItem>,
+    items: Vec<T>,
     last_selected: Option<usize>,
 }
 
@@ -27,7 +27,8 @@ struct App {
     should_quit: bool,
     focus_item: bool,
     curated_listing: Option<pandoculation::CuratedListing>,
-    display_items: StatefulList,
+    display_items: StatefulList<pandoculation::CuratedListingItem>,
+    active_item_urls: StatefulList<String>,
 }
 
 impl App {
@@ -36,7 +37,8 @@ impl App {
             should_quit: false,
             focus_item: false,
             curated_listing: None,
-            display_items: StatefulList::with_items(Vec::new()),
+            display_items: StatefulList::<pandoculation::CuratedListingItem>::with_items(Vec::new()),
+            active_item_urls: StatefulList::<String>::with_items(Vec::new()),
         }
     }
 
@@ -46,7 +48,22 @@ impl App {
 
     pub fn load_curated_listing(&mut self, curated_listing: pandoculation::CuratedListing) {
         self.curated_listing = Some(curated_listing.clone());
-        self.display_items = StatefulList::with_items(curated_listing.items.clone());
+        self.display_items = StatefulList::<pandoculation::CuratedListingItem>::with_items(curated_listing.items.clone());
+    }
+
+    pub fn load_active_item_urls(&mut self, curated_listing_item: pandoculation::CuratedListingItem) {
+
+        let mut urls: Vec<String> = Vec::new();
+
+        urls.push(curated_listing_item.data.url);
+
+        if let Some(chat_url) = &curated_listing_item.data.chat_url {
+            urls.push(chat_url.to_string());
+        }
+       
+        self.active_item_urls = StatefulList::<String>::with_items(urls.clone());
+
+        log::debug!("self.active_item_urls: {:?}", self.active_item_urls);
     }
 }
 
@@ -149,7 +166,6 @@ impl App {
             })
             .collect();
 
-        let mut state = ListState::default();
         let list = RList::new(items.clone())
             .block(Block::default().title("List").borders(Borders::ALL))
             .style(Style::default().fg(Color::White))
@@ -162,102 +178,28 @@ impl App {
     }
 
     fn render_footer(&mut self, area: Rect, buf: &mut Buffer) {
-        let selected_item: Option<pandoculation::CuratedListingItem> = if let Some(i) = self.display_items.state.selected() {
-            Some(self.display_items.items[i].clone())
-        } else {
-            None
-        };
+        let items: Vec<RListItem> = self
+            .active_item_urls
+            .items
+            .iter()
+            .map(|item| {
+                let line = Line::from(format!("{}", item))
+                    .style(Style::new().yellow());
 
-        if let Some(selected_item) = selected_item {
-            let mut lines: Vec<Line> = Vec::new();
+                return RListItem::new(line);
+            })
+            .collect();
 
-            let line_one = Line::from(format!("{}", selected_item.data.title))
-                .style(Style::new().add_modifier(Modifier::BOLD));
-            let line_two = Line::from(format!("{}", selected_item.data.url))
-                .style(Style::new().yellow());
+        //log::debug!("items: {:?}", items);
+        //log::debug!("active_item_urls: {:?}", self.active_item_urls);
 
-            lines.push(line_one);
-            lines.push(line_two);
-           
-            if let Some(chat_url) = &selected_item.data.chat_url {
-                let span_one = Span::styled(
-                    "chat: ",
-                    Style::new()
-                        .fg(Color::Yellow)
-                        .add_modifier(Modifier::BOLD)
-                );
-                let span_two = Span::styled(
-                    format!("{}", chat_url),
-                    Style::new()
-                        .fg(Color::Yellow)
-                );
-                let line = Line::from(vec![span_one, span_two]);
-                lines.push(line);
-            }
+        let list = RList::new(items.clone())
+            .block(Block::default().title("URLs").borders(Borders::ALL))
+            .style(Style::default().fg(Color::White))
+            .highlight_symbol(">>")
+            .repeat_highlight_symbol(true);
 
-            let mut additional_info: Vec<Span> = Vec::new();
-
-            if let Some(points) = &selected_item.data.points {
-                let span_one = Span::styled(
-                    "points: ",
-                    Style::new()
-                        .fg(Color::Green)
-                        .add_modifier(Modifier::BOLD)
-                );
-                additional_info.push(span_one);
-
-                let span_two = Span::styled(
-                    format!("{}", points),
-                    Style::new()
-                        .fg(Color::Green)
-                );
-                additional_info.push(span_two);
-            }
-
-            if let Some(author) = &selected_item.data.author {
-                let span_one = Span::styled(
-                    " author: ",
-                    Style::new()
-                        .fg(Color::Green)
-                        .add_modifier(Modifier::BOLD)
-                );
-                additional_info.push(span_one);
-
-                let span_two = Span::styled(
-                    format!("{}", author),
-                    Style::new()
-                        .fg(Color::Green)
-                );
-                additional_info.push(span_two);
-            }
-
-            if let Some(timestamp) = &selected_item.data.timestamp {
-                let span_one = Span::styled(
-                    " timestamp: ",
-                    Style::new()
-                        .fg(Color::Green)
-                        .add_modifier(Modifier::BOLD)
-                );
-                additional_info.push(span_one);
-
-                let span_two = Span::styled(
-                    format!("{}", timestamp),
-                    Style::new()
-                        .fg(Color::Green)
-                );
-                additional_info.push(span_two);
-            }
-
-            lines.push(Line::from(additional_info));
-
-
-            let text: Text = Text::from(lines);
-
-            Paragraph::new(text)
-                .block(Block::default().borders(Borders::ALL).title("Details"))
-                .render(area, buf);
-
-        }
+        StatefulWidget::render(list, area, buf, &mut self.active_item_urls.state);
     }
 }
 
@@ -289,8 +231,8 @@ impl Widget for &mut App {
     }
 }
 
-impl StatefulList {
-    fn with_items(items: Vec<pandoculation::CuratedListingItem>) -> StatefulList {
+impl<T> StatefulList<T> {
+    fn with_items(items: Vec<T>) -> StatefulList<T> {
         StatefulList {
             state: ListState::default(),
             items: items,
@@ -385,20 +327,44 @@ fn update(app: &mut App) -> Result<()> {
                 match key.code {
                     Char('q') => app.should_quit = true,
                     Char('j') => {
-                        if !app.focus_item {
+                        if app.focus_item {
+                            app.active_item_urls.next();
+                        } else {
                             app.display_items.next();
                         }
                     }
                     Char('k') => {
-                        if !app.focus_item {
+                        if app.focus_item {
+                            app.active_item_urls.previous();
+                        } else {
                             app.display_items.previous();
                         }
                     }
                     KeyCode::Enter => {
                         if app.focus_item {
 
+                            let selected_url: Option<String> = if let Some(i) = app.active_item_urls.state.selected() {
+                                Some(app.active_item_urls.items[i].clone())
+                            } else {
+                                None
+                            };
+
+                            if let Some(selected_url) = selected_url {
+                                log::debug!("selected_url: {}", selected_url);
+                            }
+
                         } else {
                             app.focus_item = true;
+
+                            let selected_item: Option<pandoculation::CuratedListingItem> = if let Some(i) = app.display_items.state.selected() {
+                                Some(app.display_items.items[i].clone())
+                            } else {
+                                None
+                            };
+
+                            if let Some(selected_item) = selected_item {
+                                app.load_active_item_urls(selected_item);
+                            }
                         }
                     },
                     KeyCode::Esc => {
