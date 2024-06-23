a7be46e96e0961d649d14f2b70153564fe116e69
David Roussov <david.roussov@protonmail.com>
2024-06-23 16:41:15 +1000

    change input struct definition

--- src/input.rs
+++ src/input.rs
@@ -1,9 +1,26 @@
 use serde::{Serialize, Deserialize};
 use std::collections::{HashMap};
+use itertools::Itertools;
+use std::cmp::Ordering;
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct InputMetadata {
+    pub is_id: bool,
+    pub is_url: bool,
+    pub is_page_link: bool,
+    pub is_action_link: bool,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct InputValue {
+    pub meta: InputMetadata,
+    pub name: String,
+    pub value: String,
+}
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Input {
-    pub values: HashMap<String, String>,
+    pub values: Vec<InputValue>,
     pub children: Vec<Input>,
 }
 
@@ -19,15 +36,19 @@ impl Input {
     }
 
     pub fn to_string(&self, indentation_factor: usize, result: &mut String) {
-        let mut keys: Vec<_> = self.values.keys().collect();
-        keys.sort();
-
-        if !keys.is_empty() {
+        if !self.values.is_empty() {
             let indentation = format!("{}{}", if !result.is_empty() { "\n" } else { "" }, " ".repeat(indentation_factor));
-            let values = keys.iter()
-                .fold(String::new(), |mut acc, key| {
-                    let value = self.values.get(*key).unwrap();
-                    let trimmed = value.trim();
+            let values = self.values.iter()
+                .filter(|item| {
+                    !item.meta.is_id && !item.meta.is_action_link
+                })
+                .collect::<Vec<_>>()
+                .into_iter()
+                .sorted_by(|a, b| {
+                    a.name.cmp(&b.name)
+                })
+                .fold(String::new(), |mut acc, item| {
+                    let trimmed = item.value.trim();
                     acc.push_str(&format!(" {}", trimmed));
                     acc
                 });
@@ -37,8 +58,8 @@ impl Input {
         }
 
         for child in &self.children {
-            let new_depth = if keys.is_empty() { indentation_factor } else { indentation_factor + 1 };
-            child.to_string(new_depth, result);
+            let indentation_factor = if self.values.is_empty() { indentation_factor } else { indentation_factor + 1 };
+            child.to_string(indentation_factor, result);
         }
     }
 }
